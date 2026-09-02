5afa550c7d2336f60c42ae6114b1633aafde99f0
faacclocks-glitch <faac_clocks@hotmail.com>
2026-09-02 06:49:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ def remove_from_cart(item_index):
 def index():
     if session.get('username'):
         return redirect(url_for('market'))
-    return render_template('market.html')
+    return render_template('store.html')
 
 def home():
     return redirect(render_template("home.html"))
