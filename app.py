4c4a1d5c516609f68dfe8a7aa239ab771c16609a
faacclocks-glitch <faac_clocks@hotmail.com>
2026-09-10 20:39:45 -0600

    Calculate price with a 20% markup

--- app.py
+++ app.py
@@ -1692,6 +1692,8 @@ def editar_producto_proveedor(producto_id):
             nombre = request.form.get('nombre', '').strip()
             presentacion = request.form.get('presentacion', '').strip()
             precio_proveedor = request.form.get('precio', 0)
+            precio_proveedor = float(precio_proveedor)
+            precio = round(precio_proveedor * 1.20, 2)
             unidades = request.form.get('unidades', 0)
             logistics_size = request.form.get('logistics_size', '').strip().upper()
             categoria = request.form.get('categoria', '').strip().upper()
